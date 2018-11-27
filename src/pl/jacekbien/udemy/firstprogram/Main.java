4edfe6553dcd9a597e78f5ab9c16779957eb0828
Jacek Bień <bienjacek71@gmail.com>
2018-11-27 11:12:20 +0100

    Initial commit

--- src/pl/jacekbien/udemy/firstprogram/Main.java
+++ src/pl/jacekbien/udemy/firstprogram/Main.java
@@ -2,6 +2,6 @@ package pl.jacekbien.udemy.firstprogram;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello IntelliJ!");
+        System.out.println("Jacek Bień");
     }
 }
