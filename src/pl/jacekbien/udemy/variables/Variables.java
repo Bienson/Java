7d111b3ae9277e8430d5d202c9f44daae886dfe4
Jacek Bień <bienjacek71@gmail.com>
2018-11-27 12:11:36 +0100

    Variables

---
+++ src/pl/jacekbien/udemy/variables/Variables.java
@@ -0,0 +1,38 @@
+package pl.jacekbien.udemy.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        byte b = 127;
+        byte b1 = -128;
+        short s = Short.MAX_VALUE;
+        long l = Long.MAX_VALUE;
+        Long l1 = 23L;
+        int i = Integer.MAX_VALUE;
+        Integer integer = 2;
+
+        int sum = i + 1;
+
+
+        float f = Float.MAX_VALUE;
+        float f1 = 1.5f;
+        double d = Double.MAX_VALUE;
+        double d1 = 2.1d;
+
+        char c = 'a';
+        String helloHowAreYou = "Hello, how are You?";
+
+        boolean boo = true;
+        boolean boo2 = false;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(l1);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(boo);
+        System.out.println(helloHowAreYou);
+
+    }
+}
